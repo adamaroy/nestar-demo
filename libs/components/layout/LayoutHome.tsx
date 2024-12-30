b98144f7953bec56c1b66865ab97d556315ef42f
ADAM <azizbekrustamovic@gmail.com>
2024-12-30 15:42:25 +0900

    feat:publish main layout componenets

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,30 +1,35 @@
-import Head from "next/head"
-import { Stack } from "@mui/material"
-import exp from "constants";
+import { Stack } from "@mui/material";
+import Head from "next/head";
+import Footer from "../Footer";
+import HeaderFilter from "../homepage/HeaderFilter";
+import  Top  from "./Top";
 
 const withLayoutMain = (Component: any) => {
   return (props: any) => {
-
-    return(
-    <>
-    <Head>
-      <title>Nestar</title>
-    </Head>
-    <Stack id = "pc-wrap">
-    <Stack sx={{background:"#81c784"}}>Header Home</Stack>
-
-    <Stack id={"main"}>
-        <Component {...props} />
-    </Stack>
-
-    <Stack sx={{background:"#a1887f"}}>Footer</Stack>
-
-
-    </Stack>
-    
-    </>);
+    return (
+      <>
+        <Head>
+          <title>Nestar</title>
+        </Head>
+        <Stack id="pc-wrap">
+          <Stack id="top">
+            <Top />
+          </Stack>
+          <Stack className="header-main">
+            <Stack className="container">
+              <HeaderFilter />
+            </Stack>
+          </Stack>
+          <Stack id="main">
+            <Component {...props} />
+          </Stack>
+          <Stack id="footer">
+            <Footer />
+          </Stack>
+        </Stack>
+      </>
+    );
   };
-  
 };
 
 export default withLayoutMain;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,21 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import {Box, Container, Stack} from "@mui/material";
-import exp from "constants";
-import{ NextPage } from "next";
+import { Stack } from "@mui/material";
+import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
+    <Stack className="home-page">
       <Stack>
-      <Stack flexDirection={"column"}>
-        <Stack>
-            <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack>
-            <Stack className="container">Top Agents</Stack>
-        </Stack>
-        <Stack>
-            <Stack className="container">Top Properties</Stack>
-        </Stack>
-        <Stack>
-            <Stack className="container">Events</Stack>
-        </Stack>
-        </Stack>
-      </Stack>);
+        <Stack className="container">Popular Properties</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Agents</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Properties</Stack>
+      </Stack>
+    </Stack>
+  );
 };
-export default withLayoutMain(Home);  
+
+export default withLayoutMain(Home);
