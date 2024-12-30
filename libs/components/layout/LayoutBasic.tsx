b3db16d76ed9a4af5058d802a3120a11e0bbe2d4
ADAM <azizbekrustamovic@gmail.com>
2024-12-30 15:50:53 +0900

    feat:publish basic layout componenets

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -1,29 +1,42 @@
-import Head from "next/head"
-import { Stack } from "@mui/material"
-import exp from "constants";
+import { Stack } from "@mui/material";
+import Head from "next/head";
+import Footer from "../Footer";
+import Top from "./Top";
 
 const withLayoutBasic = (Component: any) => {
   return (props: any) => {
-    return(
-    <>
-    <Head>
-      <title>Nestar</title>
-    </Head>
-    <Stack id = "pc-wrap">
-    <Stack sx={{background:"#81c784"}}>Header Basic</Stack>
-
-    <Stack id={"main"}>
-        <Component {...props} />
-    </Stack>
-
-    <Stack sx={{background:"#a1887f"}}>Footer</Stack>
-
-
-    </Stack>
-    
-    </>);
+    return (
+      <>
+        <Head>
+          <title>Nestar</title>
+        </Head>
+        <Stack id="pc-wrap">
+          <Stack id={"top"}>
+            <Top />
+          </Stack>
+          <Stack
+            className={"header-basic"}
+            style={{
+              backgroundImage: `url(/img/banner/properties.png)`,
+              backgroundSize: "cover",
+              boxShadow: "inset 10px 40px 150px 40px rgb(24 22 36)",
+            }}
+          >
+            <Stack className={"container"}>
+              <strong>Property Search</strong>
+              <span>We are glad to see you again!</span>
+            </Stack>
+          </Stack>
+          <Stack id={"main"}>
+            <Component {...props} />
+          </Stack>
+          <Stack id={"footer"}>
+            <Footer />
+          </Stack>
+        </Stack>
+      </>
+    );
   };
-  
 };
 
 export default withLayoutBasic;
